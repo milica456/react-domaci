013e3470ef6e8fcf9210020dabe0a54fb0091055
Milica Jaksic <mj20190209@student.fon.bg.ac.rs>
2023-03-21 14:11:34 +0100

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+
+ function Footer() {
+  return (
+    <div className="footer">
+        
+        <p>&copy; 2023 pizzeria.com</p>
+    </div>
+  )
+}
+export default Footer;
