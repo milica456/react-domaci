07d8e241b237d5f4fb3022388bb2cca6830e60a7
Milica Jaksic <mj20190209@student.fon.bg.ac.rs>
2023-03-21 11:10:52 +0100

    Kreiranje Navbara i ubacivanje logoa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">Hello</div>
+    <div className="App">
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={} />
+      </Switch>
+      </Router>
+      
+    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../assets/pizzaLogo.png'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'> 
+        <div className='leftSide'>
+            <img src={Logo} />
+            </div>    
+        <div className='rightSide'></div>
+    </div>
+  )
+}
